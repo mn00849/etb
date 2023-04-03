7f8247da25acbbe22cd7e1d98f519bb037f3ce97
mn00849 <mn00849@surrey.ac.uk>
2023-04-03 15:28:37 +0200

    Created Models for Backend

--- homeapp/models.py
+++ homeapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserBudget(models.Model):
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.CharField(max_length=128)
+    budget = models.DecimalField(max_digits=16,decimal_places=2)
+    startDate = models.DateField()
+    endDate = models.DateField()
+
+class fuelPrice(models.Model):
+    price = models.DecimalField(max_digits=16,decimal_places=2)
+    date = models.DateTimeField()
+
+class Routes(models.Model):
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+    cost = models.DecimalField(max_digits=16,decimal_places=2)
+    waypoints = models.CharField(max_length=512)
+    emissions = models.DecimalField(max_digits=16,decimal_places=2)
+    transportType = models.IntegerField()
+
+class transportType(models.Model):
+    type = models.CharField(max_length=128)
+
+class Friend(models.Model):
+    userOne = models.IntegerField()
+    userTwo = models.IntegerField()
