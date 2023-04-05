4a63fa2eb76fdc7290ac9a3e9577cb36c9a05853
mn00849 <mn00849@surrey.ac.uk>
2023-04-05 14:13:53 +0200

    Created function for scraping fuel prices

--- homeapp/views.py
+++ homeapp/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.globalpetrolprices.com/United-Kingdom/gasoline_prices/"
+
+def getFuel():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    prices = soup.find_all("td")
+    count = 1
+    gas = 0
+    for price in prices:
+        try:
+            thisPrice = float(price.get_text())
+            gas = thisPrice
+            break
+        except ValueError:
+            continue  
+
+    return gas
 
 # Create your views here.
 def home(request):
