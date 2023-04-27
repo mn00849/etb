35696a2a30c07b75eb4f309d2baf04b24ce55c94
st01330 <st01330@surrey.ac.uk>
2023-04-27 14:16:47 +0100

    Set/update budget

--- homeapp/urls.py
+++ homeapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
     path('logout/', views.logout, name='logout'),
+    path('set', views.set_budget, name="set")
 ]
--- homeapp/views.py
+++ homeapp/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 import requests
 from bs4 import BeautifulSoup
 from django.contrib.auth import logout as django_logout
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import UserBudget
+from .forms import BudgetForm #Name of budget setting form, to be changed
 
 url = "https://www.globalpetrolprices.com/United-Kingdom/gasoline_prices/"
 
@@ -43,3 +46,29 @@ def logout(request):
     client_id = settings.SOCIAL_AUTH_AUTH0_KEY
     return_to = 'http://127.0.0.1:8000' # this can be current domain
     return redirect(f'https://{domain}/v2/logout?client_id={client_id}&returnTo={return_to}')
+
+@login_required
+def set_budget(request):
+    context ={}
+    obj = get_object_or_404(UserBudget, id = request.user.id)
+    userID = reqest.user.id
+    if(obj == None):
+        form = BudgetForm(request.POST or None, initial={'userID':userID})
+        if(request.method == 'POST'):
+            if form.is_valid():
+                form.save()
+                messages.add_message(request, messages.SUCCESS, 'Budget has been set')
+                return redirect('budget_page', userID = userID)
+            else:
+                messages.add_message(request, messages.ERROR, 'Invalid Form Data; Budget has not been set')
+                context['form']= form
+    else:
+        if(obj.userID != request.userID):
+            raise PermisssionDenied()
+        form = BudgetForm(request.POST or None, instance = obj)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Budget Updated')
+            return redirect('budget_page', userID = userID)
+            context["form"] = form
+    return render(request, "budget_setter", context)
