f800898781b07158ae17229fd3196758299dcd28
mn00849 <mn00849@surrey.ac.uk>
2023-05-17 21:29:53 +0100

    Changed to Debug off and updated requirements.txt

--- com2027/settings.py
+++ com2027/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fvx(7kr-&h8eg47w#404s_ybo0e%)$j#dzd2s4_6rcqugqbbax'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -45,6 +45,7 @@ AUTH_USER_MODEL = 'homeapp.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
