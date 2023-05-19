b1cfdb3ce598c6087cbfb8dd893171cd829ddfa0
mn00849 <134016150+mn00849@users.noreply.github.com>
2023-05-19 11:49:48 +0100

    Update settings.py

--- com2027/settings.py
+++ com2027/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fvx(7kr-&h8eg47w#404s_ybo0e%)$j#dzd2s4_6rcqugqbbax'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
